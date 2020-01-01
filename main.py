91be163afe007c96ba51fc01703c6b84a528de12
SadScream <sadscream.ilia@gmail.com>
2020-01-02 01:47:46 +0900

    minimizing to system tray

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import fix_qt_import_error
 
 from random import choice
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from sys import exit, argv
 from design import Ui_Dialog
 from string import ascii_lowercase, ascii_uppercase, digits
@@ -19,6 +19,41 @@ class Window(QtWidgets.QMainWindow, Ui_Dialog):
 		self.generate.clicked.connect(self.generate_)
 		self.copy.clicked.connect(self.copy_)
 		self.generate.clicked.emit()
+
+		icon = QtGui.QIcon()
+		icon.addPixmap(QtGui.QPixmap(":/icons/assets/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+
+		self.tray_icon = QtWidgets.QSystemTrayIcon(self)
+		self.tray_icon.setIcon(QtGui.QIcon(icon))
+
+		show_action = QtWidgets.QAction("Показать", self)
+		hide_action = QtWidgets.QAction("Скрыть", self)
+		quit_action = QtWidgets.QAction("Закрыть", self)
+
+		show_action.triggered.connect(self.show)
+		hide_action.triggered.connect(self.hide)
+		quit_action.triggered.connect(self.closeEvent)
+
+		tray_menu = QtWidgets.QMenu()
+		tray_menu.addAction(show_action)
+		tray_menu.addAction(hide_action)
+		tray_menu.addAction(quit_action)
+
+		self.tray_icon.setContextMenu(tray_menu)
+		self.tray_icon.show()
+
+	def closeEvent(self, e):
+		try:
+			self.tray_icon.hide()
+			e.accept()
+		except:
+			e.accept()
+
+	def showEvent(self, e):
+		self.showNormal()
+
+	def hideEvent(self, e):
+		self.hide()
 
 	def copy_(self):
 		QtWidgets.QApplication.clipboard().setText(self.password.text())
